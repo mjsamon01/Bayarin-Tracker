fc79347c8a02717a26ffaf5202bdb0916e1db21c
mjsamon01 <mjsamon01@gmail.com>
2026-08-06 00:34:51 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,13 @@
 // at Realtime Database - tingnan ang README.md para sa buong guide)
 // ============================================================
 var firebaseConfig = {
-  apiKey: "PALITAN_MO_ITO",
-  authDomain: "PALITAN_MO_ITO.firebaseapp.com",
-  databaseURL: "https://PALITAN_MO_ITO-default-rtdb.firebaseio.com",
-  projectId: "PALITAN_MO_ITO",
-  storageBucket: "PALITAN_MO_ITO.appspot.com",
-  messagingSenderId: "PALITAN_MO_ITO",
-  appId: "PALITAN_MO_ITO",
+  apiKey: "AIzaSyByQ2ph_V7nxikYBc2w102jBWZt5nNe7YY",
+  authDomain: "bayarin-tracker.firebaseapp.com",
+  databaseURL: "https://bayarin-tracker-default-rtdb.firebaseio.com",
+  projectId: "bayarin-tracker",
+  storageBucket: "bayarin-tracker.firebasestorage.app",
+  messagingSenderId: "805767982695",
+  appId: "1:805767982695:web:b9199d678348448ecfeec2"
 };
 
 // Wag nang galawin ang linya sa baba
